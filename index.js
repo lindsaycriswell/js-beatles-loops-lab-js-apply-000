0ce0eb104c3c46012d0cb37816d815148d6891a7
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-09 16:17:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
-  for(let i = 0; i < musicians.length; i++)
+  for(let i = 0; i < musicians.length; i++){
     array.push(`${musicians} plays the ${instruments}.`)
+  } return array;
 }
