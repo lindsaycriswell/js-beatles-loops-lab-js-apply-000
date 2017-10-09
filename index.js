cb27a6c10133753261341c3221651f0569f4610b
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-09 16:20:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function theBeatlesPlay(musicians, instruments){
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   } return array;
 }
+
+function johnLennonFacts(facts){
+  var array = [];
+  var i = 0;
+  while(i < facts.length) {
+    array.push
+  }
+}
